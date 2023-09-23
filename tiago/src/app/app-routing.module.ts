c3f70ab41f45b645fa44a377b163c6160c3b35d8
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-09-23 08:02:16 -0300

    atual
    certo

--- tiago/src/app/app-routing.module.ts
+++ tiago/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { DestaquesComponent } from './pages/destaques/destaques.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
 
 const routes: Routes = [
 
   {
     path:'', redirectTo: 'home', pathMatch: 'full'
+  },
+
+  {
+    path:'home', component: HomeComponent
+  },
+
+  {
+    path:'destaques', component: DestaquesComponent
+  },
+
+  {
+    path:'galeria', component: GaleriaComponent
   },
 
 ];
