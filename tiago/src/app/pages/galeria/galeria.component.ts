3adddd619f272d88ef6fa4cb38a4ed47117df734
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-09-27 13:11:37 -0300

    testes

--- tiago/src/app/pages/galeria/galeria.component.ts
+++ tiago/src/app/pages/galeria/galeria.component.ts
@@ -1,11 +1,16 @@
-import { Component,  } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
   styleUrls: ['./galeria.component.scss']
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
   imageUrls: string[] = [
     '~/../assets/image/desenhos/1.jpg',
     '~/../assets/image/desenhos/2.jpg',
